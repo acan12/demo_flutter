20e16120b15e62443617bcca12b40796407f1495
acan12 <ary.suryawan@ottodigital.id>
2024-04-08 22:45:19 +0700

    update counter cubit for increment and decrement value

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:demo_flutter/v2/counter_screen.dart';
+import 'package:demo_flutter/v2/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: CounterView(),
+    return BlocProvider(
+        create: (context) => CounterCubit(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          home: CounterScreen(),
+      ),
     );
   }
 }
--- lib/v2/counter_screen.dart
+++ lib/v2/counter_screen.dart
@@ -1,19 +1,32 @@
+import 'package:demo_flutter/v2/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterView extends StatelessWidget {
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Counter App"),),
-      body: const Center(
-        child: Text('0', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),),
+      body: Center(
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text(state.toString(),
+              style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),);
+          },
+        ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          FloatingActionButton(onPressed: () {},
+          FloatingActionButton(onPressed: () {
+            context.read<CounterCubit>().increment();
+          },
             child: const Icon(Icons.add),),
-          FloatingActionButton(onPressed: () {},
+          FloatingActionButton(onPressed: () {
+            context.read<CounterCubit>().decrement();
+          },
             child: const Icon(Icons.remove),)
         ],
       ),
